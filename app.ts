a6f85ed1f849101b5b9ab46e9be3527a20a267c4
maikdevries <dev.maik.devries@outlook.com>
2025-02-19 17:03:30 +0100

    feat: define .env example configuration

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import router from './src/router.ts';
 
 Deno.serve({
-	'port': Number.parseInt(Deno.env.get('PORT') ?? ''),
+	'port': Number.parseInt(Deno.env.get('DENO_PORT') ?? ''),
 }, async (request: Request) => await router(request));
